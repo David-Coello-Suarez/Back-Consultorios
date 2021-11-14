7959b2eeb486348d69237484a6213de009894124
David Coello S <coellosuarezd@gmail.com>
2021-11-13 21:58:55 -0500

    Freezte

--- src/models/MEspecialidad.ts
+++ src/models/MEspecialidad.ts
@@ -2,29 +2,35 @@ import { literal, DataTypes } from "sequelize"
 import conexion from "../conexion/coneccion"
 import { IEspecialidad } from "../interfaces"
 
-export const Especialidades = conexion.define<IEspecialidad>("Especialidades", {
-    id: {
-        type: DataTypes.INTEGER.ZEROFILL,
-        primaryKey: true,
-        // defaultValue: 1,
-        autoIncrement: true,
-    },
-    nombre: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notNull: { msg: "El nombre es requerido" },
+export const Especialidades = conexion.define<IEspecialidad>(
+    "Especialidades",
+    {
+        id: {
+            type: DataTypes.INTEGER.ZEROFILL,
+            primaryKey: true,
+            // defaultValue: 1,
+            autoIncrement: true,
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: { msg: "El nombre es requerido" },
+            },
+        },
+        estado: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: literal("CURRENT_TIMESTAMP"),
         },
     },
-    estado: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-    },
-    createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: literal("CURRENT_TIMESTAMP"),
-    },
-})
+    {
+        freezeTableName: true,
+    }
+)
 
-Especialidades.sync({force: true})
+Especialidades.sync()
